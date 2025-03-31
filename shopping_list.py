25eced29e2aa2c800882bc644b3c2b758dbf0e54
Xameshi <lipskee@o2.pl>
2025-03-31 14:36:23 +0200

    Add total products count

--- shopping_list.py
+++ shopping_list.py
@@ -9,3 +9,7 @@ for sklep, produkty in zakupy.items():
     sklep = sklep.capitalize()
     produkty_formatowane = [produkt.capitalize() for produkt in produkty]
     print(f"Idę do {sklep}, kupuję tu następujące rzeczy: {', '.join(produkty_formatowane)}")
+
+
+liczba_produktów = sum(len(produkty) for produkty in zakupy.values())
+print(f"W sumie kupuję {liczba_produktów} produktów.")
