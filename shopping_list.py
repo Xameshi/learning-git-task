ee108b0d2db75506c111a8720f6f5dd7b6d4e370
Xameshi <lipskee@o2.pl>
2025-03-31 14:35:16 +0200

    Print shopping list summary

--- shopping_list.py
+++ shopping_list.py
@@ -2,3 +2,10 @@ zakupy = {
     "Piekarnia": ["Chleb", "Pączek", "Bułki"],
     "Warzywniak": ["Marchew", "Seler", "Rukola"]
 }
+print("Lista zakupów:", zakupy)
+
+
+for sklep, produkty in zakupy.items():
+    sklep = sklep.capitalize()
+    produkty_formatowane = [produkt.capitalize() for produkt in produkty]
+    print(f"Idę do {sklep}, kupuję tu następujące rzeczy: {', '.join(produkty_formatowane)}")
